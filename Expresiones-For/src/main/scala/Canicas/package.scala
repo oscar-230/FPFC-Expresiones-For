3ed005f8f2193307b7dfb88cbb384d5912d011f0
Oscar Cuaical <cuaical.oscar@correounivalle.edu.co>
2024-04-16 15:26:40 -0500

    Se crea la estructura para la implementacion

---
+++ Expresiones-For/src/main/scala/Canicas/package.scala
@@ -0,0 +1,27 @@
+package object Canicas {
+
+  type Frasco = (Int, Int)
+  type Distr = List[Frasco]
+
+  /*
+  def canicasPosiblesFrasco(f: Int, c: Int): List[Frasco] = {
+
+  }
+
+  def canicasPorFrasco(n: Int, c: Int): List[Distr] = {
+
+  }
+
+  def mezclarLCanicas(lc:List[Distr]): List[Distr] = {
+
+  }
+
+  def distribucion(m:Int, n:Int, c:Int):List[Distr] = {
+
+  }
+
+  def agrupaciones(m: Int): List[List[Int]] = {
+
+  }
+  */
+}
