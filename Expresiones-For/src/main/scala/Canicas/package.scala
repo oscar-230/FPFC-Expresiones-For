86a8797e968772cb6f046dfefb69b51a686113df
Jose Fuertes <147567605+Hosesote@users.noreply.github.com>
2024-04-21 17:55:50 -0500

    funciones distribucion y agrupaciones

--- Expresiones-For/src/main/scala/Canicas/package.scala
+++ Expresiones-For/src/main/scala/Canicas/package.scala
@@ -3,22 +3,15 @@ package object Canicas {
   type Frasco = (Int, Int)
   type Distr = List[Frasco]
 
-
   def canicasPosiblesFrasco(f: Int, c: Int): List[Frasco] = {
-    for {
-      canicas <- (0 to c).toList
-    } yield (f, canicas)
+    (0 to c).toList.map(canicas => (f, canicas))
   }
-
 
   def canicasPorFrasco(n: Int, c: Int): List[Distr] = {
-    for {
-      f <- (1 to n).toList
-    } yield canicasPosiblesFrasco(f, c)
+    (1 to n).toList.map(f => canicasPosiblesFrasco(f, c))
   }
 
-
-  def mezclarLCanicas(lc:List[Distr]): List[Distr] = {
+  def mezclarLCanicas(lc: List[Distr]): List[Distr] = {
     lc match {
       case Nil => List(Nil)
       case x :: xs => for {
@@ -28,14 +21,24 @@ package object Canicas {
     }
   }
 
-
-  /*
-  def distribucion(m:Int, n:Int, c:Int):List[Distr] = {
-
+  def distribucion(m: Int, n: Int, c: Int): List[Distr] = {
+    val canicas = canicasPorFrasco(n, c)
+    val combinaciones = mezclarLCanicas(canicas)
+    combinaciones.filter(_.map(_._2).sum == m)
   }
 
   def agrupaciones(m: Int): List[List[Int]] = {
+    def combinar(n: Int, lista: List[Int]): List[List[Int]] = {
+      if (n == 0) List(Nil)
+      else {
+        for {
+          i <- lista
+          j <- combinar(n - i, lista.filter(_ > i))
+        } yield i :: j
+      }
+    }
 
+    val listaElementos = (1 to m).toList
+    combinar(m, listaElementos)
   }
-  */
 }
